27ad6b45fe9524957ce639b5af0d7b866d48761a
placidaxid <nwanagaplacid@gmail.com>
2022-05-01 23:45:44 -0500

    corrections made and big mistake seen

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-        var weight = parseInt(prompt('please enter your weight'))
+var weight = parseInt(prompt('please enter your weight'))
         var height = parseFloat(prompt('please enter your height'))
         function bmiAdvanced(weight, height){
             var bmi = weight /(height * height);
-            var bmiRound = Math.round(bmi); }
-        if (bmiRound < 18.5) {
-            var output = `Your BMI is ${bmiRound} so you are underweight`;
-            } else if (bmiRound = 18.5 - 24.9) {
-            var output = `Your BMI is ${bmiRound}, so you have a normal weight`;
-            } else {
+            var bmiRound = Math.round(bmi); 
+            var output;
+        if(bmiRound < 18.5) {
+            var output = `Your BMI is ${bmiRound} so you are underweight.`;
+            }else if(bmiRound >= 18.5 && 24.9){
+            var output = `Your BMI is ${bmiRound}, so you have a normal weight.`;
+            }else if(bmiRound > 24.9 ) 
             var output = `Your BMI is ${bmiRound}, so you are overweight`;
-        function alert(output);
-            }
-        bmiAdvanced(weight, height);
+            //return output;
+            alert(output);
+        }
+        bmiAdvanced(65, 1.8)
